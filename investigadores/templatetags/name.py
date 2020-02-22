a631093599143ca0c2a1b6227e297ec55b548474
3000alex <2013rex@gmail.com>
2020-02-22 00:56:43 -0600

    Entrar

--- investigadores/templatetags/name.py
+++ investigadores/templatetags/name.py
@@ -7,3 +7,8 @@ def nombreCorto(cadenaAutores,nombreC):
     nombre = nombreC.split(",")
     autores = cadenaAutores.replace(nombre[0],'<strong>{nombre}</strong>')
     return str(autores.format(nombre=nombre[0]))
+
+@register.filter(name='nombreFile')
+def nombreFile(nombreFile):
+    nombre = nombreFile.replace("anexos/","")
+    return nombre
