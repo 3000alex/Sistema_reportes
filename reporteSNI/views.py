e8306610af989a9abb118df34c9b804440f0d869
3000alex <2013rex@gmail.com>
2020-03-08 01:26:48 -0600

    Entrar

--- reporteSNI/views.py
+++ reporteSNI/views.py
@@ -7,7 +7,7 @@ from django.views.generic import View, ListView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from biblioteca.models import Biblioteca
-#from SNIads import SNIads
+from SNIads import SNIads
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 token = "71EV2aJvIIiFZLSoA9cWRlxgjxTQKwykjEi3yQS7"
